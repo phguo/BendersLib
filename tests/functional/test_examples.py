751c3c3828f53ab7ec5aa7b6e3e2fc9cb008d7d7
Penghui Guo <m@guo.ph>
2026-05-04 15:54:49 +0800

    test: refactor example file filtering in tests

--- tests/functional/test_examples.py
+++ tests/functional/test_examples.py
@@ -10,8 +10,8 @@ import pytest
 EXAMPLES_DIR = Path(__file__).parent.parent.parent / "examples"
 example_files = [f for f in EXAMPLES_DIR.glob("**/*.py") if f.name != "__init__.py"]
 
-if sys.version_info >= (3, 13) and sys.version_info < (3, 14):
-    example_files = [f for f in example_files if not 'scip' in f.name and not 'cplex' in f.name]
+# if sys.version_info >= (3, 13) and sys.version_info < (3, 14):
+example_files = [f for f in example_files if 'scip' not in f.name and 'cplex' not in f.name]
 
 
 @pytest.mark.parametrize(
