ec4745d30e113fa8118b040c85e8ec2b0baf76d2
Penghui Guo <m@guo.ph>
2026-05-04 15:03:05 +0800

    test: simplify filtering of CPLEX for Python 3.13

--- examples/iis/iis_cplex.py
+++ examples/iis/iis_cplex.py
@@ -41,16 +41,15 @@ def make_sub_problem():
 
 
 if __name__ == '__main__':
-    if not sys.version_info >= (3, 13) and sys.version_info < (3, 14):
-        sub = make_sub_problem()
+    sub = make_sub_problem()
 
-        # For simplicity, we directly use the Cplex solver interface from BendersLib.
-        # Typically, this should be wrapped in the SubProblem class like SubProblem(Cplex(...)).
-        sub_problem_solver = Cplex(sub)
+    # For simplicity, we directly use the Cplex solver interface from BendersLib.
+    # Typically, this should be wrapped in the SubProblem class like SubProblem(Cplex(...)).
+    sub_problem_solver = Cplex(sub)
 
-        sub_problem_solver.solve()
-        iis_vars = sub_problem_solver.compute_iis()
-        print("Variables involved in the IIS:", iis_vars)
+    sub_problem_solver.solve()
+    iis_vars = sub_problem_solver.compute_iis()
+    print("Variables involved in the IIS:", iis_vars)
 
 # %%
 #
--- tests/functional/test_examples.py
+++ tests/functional/test_examples.py
@@ -11,7 +11,7 @@ EXAMPLES_DIR = Path(__file__).parent.parent.parent / "examples"
 example_files = [f for f in EXAMPLES_DIR.glob("**/*.py") if f.name != "__init__.py"]
 
 if sys.version_info >= (3, 13) and sys.version_info < (3, 14):
-    example_files = [f for f in example_files if not 'scip' in f.name]
+    example_files = [f for f in example_files if not 'scip' in f.name and not 'cplex' in f.name]
 
 
 @pytest.mark.parametrize(
