d6544c8229c1628335fa0e1c1163871f70b62c80
Penghui Guo <m@guo.ph>
2026-05-29 02:33:22 +0800

    docs(examples): unify benchmark file names

--- examples/benchmarks/lbbd_location.py
+++ examples/benchmarks/lbbd_location.py
@@ -3,7 +3,7 @@
 # Copyright (c) 2021-2026 Peng-Hui Guo <m@guo.ph>
 
 """
-Facility Location
+Facility Location (Gurobi)
 =======================================================
 
 This example implements the Capacity- and Distance-Constrained Plant Location Problem. The problem
--- examples/benchmarks/linear.py
+++ examples/benchmarks/linear.py
@@ -3,7 +3,7 @@
 # Copyright (c) 2021-2026 Peng-Hui Guo <m@guo.ph>
 
 """
-Linear Recourse
+Linear Recourse (Gurobi)
 =======================================================
 """
 
